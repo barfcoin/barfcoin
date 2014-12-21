d39c80ec99ce4582f9d6bd11f74a7b862aa2787e
barfcoin <barfcoin@hushmail.com>
2014-12-21 16:51:50 +0100

    changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -38,9 +38,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("BRF");
+    case mBTC: return QString("mBRF");
+    case uBTC: return QString::fromUtf8("μBRF");
     default: return QString("???");
     }
 }
@@ -49,9 +49,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Barfcoins");
+    case mBTC: return QString("Milli-Barfcoins (1 / 1,000)");
+    case uBTC: return QString("Micro-Barfcoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
